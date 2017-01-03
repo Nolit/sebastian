05c9bba26fb33d6348c741a2a242446201446500
Nolit <yaboonjapan@gmail.com>
2017-01-04 00:02:18 +0900

    Hello World表示処理作成

---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by yaboo on 2017/01/03.
+  */
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello World!")
+  }
+
+  def say(name: String) = s"Hello, $name"
+}
---
+++ src/test/scala/HelloWorldTest.scala
@@ -0,0 +1,7 @@
+import org.scalatest.FunSuite
+
+class HelloWorldTest extends FunSuite {
+  test("Hello, <引数> という文字列が返ってくる") {
+    assert(HelloWorld.say("Scala") == "Hello, Scala")
+  }
+}
