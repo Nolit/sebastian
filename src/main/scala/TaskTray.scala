82d491c50e37f58366464fa3c368a860b778e052
Nolit <yaboonjapan@gmail.com>
2017-01-04 19:02:37 +0900

    リテラルをフィールドに移動

--- src/main/scala/TaskTray.scala
+++ src/main/scala/TaskTray.scala
@@ -6,10 +6,12 @@ import javax.imageio.ImageIO
   * Created by yaboo on 2017/01/04.
   */
 object TaskTray {
+  private val iconPath = "src/main/resources/img/icon.png"
+  
   def show: Unit ={
     println("put on task tray...")
 
-    val icon: TrayIcon = new TrayIcon(ImageIO.read(new File("src/main/resources/img/icon.png")))
+    val icon: TrayIcon = new TrayIcon(ImageIO.read(new File(iconPath)))
     SystemTray.getSystemTray.add(icon)
   }
 
