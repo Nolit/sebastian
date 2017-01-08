3355db350f153d2694b680c292eefe3eb163431b
LinuStock <linustocks1969@gmail.com>
2017-01-09 03:46:38 +0900

    add scalaFX sample

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ version := "1.0"
 
 scalaVersion := "2.12.1"
 
-libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
+libraryDependencies ++= Seq(
+  "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
+  "org.scalafx" %% "scalafx" % "8.0.102-R11"
+)
--- src/main/scala/Pomodoro.scala
+++ src/main/scala/Pomodoro.scala
@@ -1,8 +1,26 @@
 /**
   * Created by Linustocks on 2017/01/06.
   */
-object Pomodoro {
-  def main(args: Array[String]) {
-    println("welcome pomodoro!")
+//package example.lambda
+
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.control.Button
+import scalafx.scene.layout.VBox
+
+object Pomodoro extends JFXApp {
+  stage = new PrimaryStage {
+    title = "Hello"
+    scene = new Scene {
+      root = new VBox {
+        children = new Button("Hello Button") {
+          onMouseClicked = handle {
+            println("hello")
+          }
+        }
+      }
+    }
   }
 }
